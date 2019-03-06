c3729c0e61df7a8694692fdc8e7e801ff6e90e8b
Mike Samuels <samuelsmichael222@gmail.com>
2019-03-06 11:18:01 -0600

    Fix to bug whereby Lot, Lane, and other fields were getting blanked out whenever user went from Applications Information tab to Documents tab, and then directly back to Applications Information tab.

--- SubmittalProposal/Submittal2.aspx.designer.cs
+++ SubmittalProposal/Submittal2.aspx.designer.cs
@@ -166,6 +166,15 @@ namespace SubmittalProposal {
         protected global::System.Web.UI.WebControls.Button btnProjectConditionsTrigger;
         
         /// <summary>
+        /// btnDocumentsTrigger control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDocumentsTrigger;
+        
+        /// <summary>
         /// TabContainer1 control.
         /// </summary>
         /// <remarks>
